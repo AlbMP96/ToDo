a5efefb9ed76dbaf0933d5b035cf7f3a4296ab98
Alberto Muñiz <92947424+AlbMP96@users.noreply.github.com>
2025-07-30 00:23:27 +0200

    basic form styles

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -5,6 +5,7 @@
 
 	let email: string = '';
 	let password: string = '';
+	let showPassword: boolean = false;
 
 	async function sendLogin() {
 		try {
@@ -16,16 +17,66 @@
 	}
 </script>
 
-<div>
+<div class="login-page">
 	<form on:submit|preventDefault={sendLogin}>
-		<label>
+		<label class="email-field" for="email">
 			Email:
-			<input type="email" bind:value={email} required />
+			<input type="email" id="email" bind:value={email} required />
 		</label>
-		<label>
-			Password:
-			<input type="password" bind:value={password} required />
-		</label>
+		<div class="password-field">
+			<label for="password">
+				Password:
+				<input
+					type={showPassword ? 'text' : 'password'}
+					id="password"
+					bind:value={password}
+					required
+				/>
+			</label>
+			<label for="show-pass">
+				Show Password
+				<input type="checkbox" bind:checked={showPassword} name="show-pass" id="show-pass" />
+			</label>
+		</div>
 		<ButtonPrimary type="submit">Login</ButtonPrimary>
 	</form>
 </div>
+
+<style lang="scss">
+	.login-page {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+
+		form {
+			background-color: oklch(86.9% 0.005 56.366);
+			display: flex;
+			flex-direction: column;
+			gap: 1rem;
+			margin: auto;
+			padding: 2rem;
+			border-radius: 10px;
+			box-shadow: 0 0 10px oklch(70.9% 0.01 56.259);
+		}
+
+		.email-field,
+		.password-field > label:first-child {
+			display: flex;
+			flex-direction: column;
+		}
+
+		.password-field {
+			display: flex;
+			flex-direction: column;
+			gap: 0.5rem;
+		}
+
+		input {
+			padding: 0.5rem;
+			border: 1px solid #ccc;
+			border-radius: 4px;
+		}
+	}
+</style>
